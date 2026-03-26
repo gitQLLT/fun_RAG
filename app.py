b9524350277e368f3a09764143a4576bc4eed3c8
liangtao <1294483828@qq.com>
2026-03-26 16:13:25 +0800

    修改提交

---
+++ app.py
@@ -0,0 +1,31 @@
+import gradio as gr
+from RAG.VectorBase import VectorStore
+from RAG.utils import ReadFiles
+from RAG.LLM import SiliconflowChat
+from RAG.Embeddings import BgeWithAPIEmbedding
+
+#输入文本处理程序
+def greet(name):
+  # 没有保存数据库
+  docs = ReadFiles('./data').get_content(max_token_len=600, cover_content=150) # 获得data目录下的所有文件内容并分割
+  vector = VectorStore(docs)
+  embedding = BgeWithAPIEmbedding() # 创建EmbeddingModel
+  vector.get_vector(EmbeddingModel=embedding)
+  vector.persist(path='storage') # 将向量和文档内容保存到storage目录下，下次再用就可以直接加载本地的数据库
+
+  # vector.load_vector('./storage') # 加载本地的数据库
+
+  content = vector.query(name, EmbeddingModel=embedding, k=1)[0]
+  chat = SiliconflowChat()
+  return chat.chat(name, [], content)
+
+#接口创建函数
+#fn设置处理函数，inputs设置输入接口组件，outputs设置输出接口组件
+#fn,inputs,outputs都是必填函数
+demo = gr.Interface(fn=greet, inputs=gr.Textbox(
+        lines=5,
+        autofocus=True
+    ), outputs=gr.Textbox(
+        lines=5
+    ))
+demo.launch()
